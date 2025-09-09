080fb4ff7cbc66878f005ad461462c635cbc38a3
Sam Arul Raj (Sam) <samarulraj@gmail.com>
2025-09-09 22:06:12 +0530

    Create login.guard.ts

---
+++ login.guard.ts
@@ -0,0 +1,17 @@
+// src/app/auth/login.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class LoginGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['/dashboard']); // already logged in → dashboard
+      return false;
+    }
+    return true; // not logged in → allow access to login hero
+  }
+}
